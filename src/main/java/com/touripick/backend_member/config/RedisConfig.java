21f94cb46a29c11ef76a0eb541fc937fd6fb2ea0
wkddnjs9 <78907516+wkddnjs9@users.noreply.github.com>
2025-06-25 00:52:57 +0900

    redis

--- src/main/java/com/touripick/backend_member/config/RedisConfig.java
+++ src/main/java/com/touripick/backend_member/config/RedisConfig.java
@@ -1,5 +1,6 @@
 package com.touripick.backend_member.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -8,6 +9,12 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
+
+    @Value("${spring.data.redis.host}")
+    private String host;
+
+    @Value("${spring.data.redis.port}")
+    private int port;
 
     @Bean
     public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory connectionFactory) {
